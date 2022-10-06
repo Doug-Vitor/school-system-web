2eb1493338484964cb8694b19dd11723ad7d5ba1
Douglas <douglasvitor.dev@gmail.com>
2022-10-06 19:41:08 -0300

    Setting tailwind root variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,27 @@ module.exports = {
     './src/pages/**/*.tsx',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { main: '#0e00ad' },
+        primary: { main: '#1300e4' },
+        text: { main: '#fff' },
+        shadow: { main: '#c2c2c2' },
+
+        success: {
+          main: '#00a508',
+          hover: '#00c00a'
+        },
+        danger: {
+          main: '#b40000',
+          hover: '#ff0000'
+        },
+        info: {
+          main: '#0e00ad',
+          hover: '#1300e4'
+        }
+      }
+    },
   },
   plugins: [],
 }
