915e03862e52a1429380475d856cef6c7ae53155
Douglas <douglasvitor.dev@gmail.com>
2022-10-06 20:07:21 -0300

    Adding classes to main element

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 import App from './App'
+import './index.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(<Provider store={store}><App /></Provider>);
