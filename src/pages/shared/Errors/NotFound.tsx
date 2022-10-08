61a4e1b7562596d418e7665e8297af49d4d53cdc
Douglas <douglasvitor.dev@gmail.com>
2022-10-08 14:47:45 -0300

    Applying tailwind to not found error page

--- src/pages/shared/Errors/NotFound.tsx
+++ src/pages/shared/Errors/NotFound.tsx
@@ -1,6 +1,6 @@
 export default () =>
-    <article>
-        <h2 className='text-danger'>Algo deu errado!</h2>
+    <article className="flex flex-col items-center font-bold">
+        <h2 className='text-danger-main mb-[4.5vh] text-xl'>Algo deu errado!</h2>
         <img src="https://img.icons8.com/external-dreamstale-lineal-dreamstale/64/000000/external-page-not-found-applications-dreamstale-lineal-dreamstale.png" alt='Pagina não encontrada' />
         <h3>Aparentemente, essa página não existe em nosso site.</h3>
     </article>
