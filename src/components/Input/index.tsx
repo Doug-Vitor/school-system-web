4e2fcdb1a3427fb4e3d76adddfbb58440ae64cda
Douglas <douglasvitor.dev@gmail.com>
2022-10-08 23:39:24 -0300

    Datetime picker CSS fixes

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,14 @@
 import IInputProperties from "./common/IProperties";
 
+import { input, label } from "../../styles/form";
 import FormGroup from "./common/FormGroup";
 import Select from "./Select";
 import DatePicker from "./DatePicker";
 
 const Input = (props: IInputProperties) =>
     <FormGroup>
-        <label className="w-[100%]">{props.labelValue}</label>
-        <input type={props.type ?? "text"} className="transition ease-in p-[9px] w-[100%] mx-[0.6vw] bg-transparent border-0 border-b-[1px] border-black outline-none focus:rounded-[4px] focus:border-none focus:shadow-lg" disabled={props.disabled ?? false} value={props.value} onChange={props.onChange} />
+        <label className={label}>{props.labelValue}</label>
+        <input type={props.type ?? "text"} className={input} disabled={props.disabled ?? false} value={props.value} onChange={props.onChange} />
     </FormGroup>;
 
 export default Input;
