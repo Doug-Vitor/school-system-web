a3d5e8dfbfd8b58a7b3de476895b0bdffc0e49d4
Douglas <douglasvitor.dev@gmail.com>
2022-10-16 14:18:36 -0300

    Creating component to store main configurations

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { publicRoutes, privateRoutes } from './routes';
 
 import Protected from './components/Area/Protected';
+import Configurations from './components/Area/Configurations';
 import Loader from './components/Loader';
 
 import Navbar from './components/Navbar';
@@ -10,16 +11,18 @@ import Home from './pages/Home';
 
 export default () =>
     <BrowserRouter>
-        <Loader />
-        <Navbar />
+        <Configurations>
+            <Loader />
+            <Navbar />
 
-        <main>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                {publicRoutes.map(mapPublicRoutes)}
-                {privateRoutes.map(mapProtectedRoutes)}
-            </Routes>
-        </main>
+            <main>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    {publicRoutes.map(mapPublicRoutes)}
+                    {privateRoutes.map(mapProtectedRoutes)}
+                </Routes>
+            </main>
+        </Configurations>
     </BrowserRouter>;
 
 const mapPublicRoutes = (({ path, element }: any, key: any) => <Route path={path} element={element} key={key} />)
