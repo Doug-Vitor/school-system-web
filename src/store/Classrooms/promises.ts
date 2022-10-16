542f286fdea1d2a2db799dc0c5d117b6f872f330
Douglas <douglasvitor.dev@gmail.com>
2022-10-16 14:19:08 -0300

    Aligning redux subjects/classrooms promises with new generic data access

--- src/store/Classrooms/promises.ts
+++ src/store/Classrooms/promises.ts
@@ -2,23 +2,11 @@ import { AppDispatch } from "..";
 import { getAll } from ".";
 
 import { get } from "../../../api/GenericDataAccess";
-import { onRequest, onRequestFinished } from "../Loading";
-
-import IDefaultRequestPayload from "../../../core/Interfaces/API/Requests/IDefaultRequestPayload";
 import IClassroom from '../../../core/Interfaces/Entities/Core/IClassroom';
 
 const BASE_URL = 'classrooms/';
 
-const requestPayload: IDefaultRequestPayload = {
-    url: BASE_URL,
-    protectedArea: true
-}
-
-export const getAllAsync = (): any => {
-    return async function (dispatch: AppDispatch) {
-        dispatch(onRequest());
-        get<IClassroom[]>(requestPayload).then(response => {
-            dispatch(getAll(response.data));
-        }).finally(() => dispatch(onRequestFinished()));
-    }
+export const getAllAsync = (): any => async (dispatch: AppDispatch) => {
+    const { data } = await get<IClassroom[]>({ url: BASE_URL, protectedArea: true });
+    dispatch(getAll(data));
 }
--- src/store/Subjects/promises.ts
+++ src/store/Subjects/promises.ts
@@ -2,23 +2,11 @@ import { getAll } from ".";
 import { AppDispatch } from "..";
 
 import { get } from "../../../api/GenericDataAccess";
-import { onRequest, onRequestFinished } from "../Loading";
-
-import IDefaultRequestPayload from "../../../core/Interfaces/API/Requests/IDefaultRequestPayload";
 import ISubject from "../../../core/Interfaces/Entities/Core/ISubject";
 
 const BASE_URL = 'subjects/';
 
-const requestPayload: IDefaultRequestPayload = {
-    url: BASE_URL,
-    protectedArea: true
-}
-
-export const getAllAsync = (): any => {
-    return async function (dispatch: AppDispatch) {
-        dispatch(onRequest());
-        get<ISubject[]>(requestPayload).then(response => {
-            dispatch(getAll(response.data))
-        }).finally(() => dispatch(onRequestFinished()));
-    }
+export const getAllAsync = (): any => async (dispatch: AppDispatch) => {
+    const { data } = await get<ISubject[]>({ url: BASE_URL, protectedArea: true });
+    dispatch(getAll(data));
 }
