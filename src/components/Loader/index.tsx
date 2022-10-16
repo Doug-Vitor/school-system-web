3c0de65a9dd02bc9d3a9829a6e9917313a097ed7
Douglas <douglasvitor.dev@gmail.com>
2022-10-16 14:02:53 -0300

    Applying tailwind and css file to loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,7 +1,13 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 
+import './index.css';
 export default () => {
     const { isLoading } = useSelector((state: RootState) => state.loading);
-    return <div className={`loader ${isLoading ? '' : 'disabled'}`}><span className="loader"></span></div>
+
+    return (
+        <div className={`flex absolute justify-center items-center w-[100%] h-[100%] pb-[80px] bg-[#bababa] opacity-50 ${!isLoading && 'hidden'}`}>
+            <span className="w-[50px] h-[50px] border-[3px] border-solid border-white rounded-[50%] border-t-[#252121] loader"></span>
+        </div>
+    );
 }
